44ce566249e78b54ba97a04f0ff0419e143d055a
Diego Attard <diegoattard@gmail.com>
2025-06-04 17:52:03 -0400

    Adjusted parameter names in the ALU module to reflect modified naming in the defenition package.

--- ALU.sv
+++ ALU.sv
@@ -1,4 +1,4 @@
-import alu_defs::*;
+import cpu_defs::*;
 
 module ALU (
 	input logic [31:0] a,
@@ -10,19 +10,19 @@ module ALU (
 	// computes alu instructions
 	always_comb begin
 		case (alu_op)
-			ADD: result = a + b;
-			SUB: result = a - b;
-			AND: result = a & b;
-			OR: result = a | b;
-			XOR: result = a ^ b;
-			SLL: result = a << b[4:0];
-			SRL: result = a >> b[4:0];
-			SRA: result = $signed(a) >>> b [4:0];
-			BEQ: result = (a == b) ? 32'd1 : 32'd0; // outputs 1 if equal and 0 otherwise
-			BNE: result = (a != b) ? 32'd1 : 32'd0; // outputs 1 if not equal and 0 otherwise
-			BLT: result = (a < b) ? 32'd1 : 32'd0; // outputs 1 if less than and 0 otherwise
-			BGE: result = (a >= b) ? 32'd1 : 32'd0; // outputs 1 if greater than or equal to and 0 otherwise
-			NOP: result = 32'd0;
+			ALU_ADD: result = a + b;
+			ALU_SUB: result = a - b;
+			ALU_AND: result = a & b;
+			ALU_OR: result = a | b;
+			ALU_XOR: result = a ^ b;
+			ALU_SLL: result = a << b[4:0];
+			ALU_SRL: result = a >> b[4:0];
+			ALU_SRA: result = $signed(a) >>> b [4:0];
+			ALU_BEQ: result = (a == b) ? 32'd1 : 32'd0; // outputs 1 if equal and 0 otherwise
+			ALU_BNE: result = (a != b) ? 32'd1 : 32'd0; // outputs 1 if not equal and 0 otherwise
+			ALU_BLT: result = (a < b) ? 32'd1 : 32'd0; // outputs 1 if less than and 0 otherwise
+			ALU_BGE: result = (a >= b) ? 32'd1 : 32'd0; // outputs 1 if greater than or equal to and 0 otherwise
+			ALU_NOP: result = 32'd0;
 			default: result = 32'd0;
 		endcase
 	end
