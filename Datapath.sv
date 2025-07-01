8ddcb56680794a3ad323fd3390b29e6fc4e83409
Diego Attard <diegoattard@gmail.com>
2025-07-01 16:01:04 -0400

    Added comments to datapath module for clarity.

--- Datapath.sv
+++ Datapath.sv
@@ -5,6 +5,7 @@ module Datapath(
 	input logic reset
 );
 	
+	// internal signal declarations
 	logic [31:0] PC;
 	logic [31:0] instruction;
 	logic [31:0] immediate;
@@ -21,9 +22,13 @@ module Datapath(
 	logic isBranch;
 	logic branch_taken;
 	
+	// checking if a branch is taken
 	assign branch_taken = isBranch && (ALU_result == 1);
+	
+	// computing next pc value
 	assign PC_next = (isJump || branch_taken) ? ALU_result : PC + 4;
 	
+	// using ALU module to perform arithmetic/logical operations
 	ALU ALU_inst(
 		.a(rs1),
 		.b((ALU_src) ? immediate : rs2),
@@ -31,6 +36,7 @@ module Datapath(
 		.result(ALU_result)
 	);
 	
+	// using program counter module to hold the address of the current instruction
 	ProgramCounter PC_inst(
 		.clock(clock),
 		.reset(reset),
@@ -38,6 +44,7 @@ module Datapath(
 		.pc_out(PC)
 	);
 	
+	// using register file module to read and write from registers
 	RegisterFile register_inst(
 		.clock(clock),
 		.reset(reset),
@@ -50,12 +57,13 @@ module Datapath(
 		.readData2(rs2)
 	);
 	
+	// using instruction memory module to fetch instructions from memory
 	InstructionMemory instruction_memory_inst(
 		.curr_addr(PC),
 		.instruction(instruction)
 	);
 	
-	
+	// using control unit module to decode instructions and generate appropriate control signals
 	ControlUnit control_unit_inst(
 		.instruction(instruction),
 		.regWrite(regWrite),
@@ -67,11 +75,13 @@ module Datapath(
 		.ALU_op(ALU_op)
 	);
 	
+	// using immediate generator module to extract immediate values from instructions
 	ImmediateGenerator immediate_inst(
 		.instruction(instruction),
 		.immediate(immediate)
 	);
 	
+	// using data module for load and store memory instructions
 	DataMemory data_memory_inst(
 		.clock(clock),
 		.write_enable(memWrite),
